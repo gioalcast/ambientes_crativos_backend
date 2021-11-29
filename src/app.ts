0d771818bb13abba30d731fe5d7511135392a900
gioalcast <gioalcast@gmail.com>
2021-11-28 23:04:19 -0500

    segundo commit

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,31 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res)=>{
-    res.send('Pueba del servido');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.get('/prueba/:id', (req, res, next)=>{
+
+    console.log('Antes de la promesa')
+
+    const x = 10;
+    const promesa = new Promise ((resolve, reject) => {
+        if (x == 10){
+            resolve('Se resuelve promesa')
+        } else {
+            reject ('Promesa rechazada');
+        }
+    });
+
+    promesa.then((res) =>{
+        console.log (res);
+    }).catch(error => {
+        console.log(error)
+    });
+
+    console.log('Despues de la promesa')
+
+    res.status(201).json({message: "Datos creados"});
 });
 
 app.listen(port, () => {
